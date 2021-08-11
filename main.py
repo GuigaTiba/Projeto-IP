899f1dfb8c11f614607737eea279f454a07deb11
Icaro Nunes <icarolimanunes@gmail.com>
2021-08-11 19:46:34 -0300

    Gera um desenho com um quadrado no canto

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-# main codigo
+import pygame
+pygame.init()
+
+
+
+screen = pygame.display.set_mode([500, 500])
+
+
+running = True
+while running:
+    
+    for event in pygame.event.get():
+        
+        if event.type == pygame.QUIT:
+            running = False
+    
+    screen.fill((255, 255, 255))
+
+    pygame.draw.rect(screen, (25, 25, 25), (30, 450, 50, 50), 0)
+
+    pygame.display.flip()
+
+pygame.quit()
